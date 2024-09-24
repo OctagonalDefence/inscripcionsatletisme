bdc8a4ae7c57be960f34327e9a6c35d46e251b87
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-09-24 19:59:03 +0200

    fieldset1 tests

--- inscripcionsatletisme/src/app/Components/inscripcio/inscripcio.component.spec.ts
+++ inscripcionsatletisme/src/app/Components/inscripcio/inscripcio.component.spec.ts
@@ -20,4 +20,48 @@ describe('InscripcioComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it ("should have a h1 called Inscripcio de proves d'atletisme", () => {
+        //h1
+        const h1 = fixture.nativeElement.querySelector('h1');
+        expect(h1.textContent).toEqual("Inscripcio de proves d'atletisme");
+
+  });
+
+  it('should validate all the inputs in the first fieldset with its conditions', () => {
+
+    //form
+    const form = fixture.nativeElement.querySelector('form');
+    expect(form).toBeTruthy();
+
+    //fieldset1
+    const fieldset1 = fixture.nativeElement.querySelector('fieldset');
+    expect(fieldset1).toBeTruthy();
+
+    //h3
+    const h3 = fixture.nativeElement.querySelector('h3');
+    expect(h3.textContent).toEqual("Dades identificatives");
+
+    //DNI
+    const dni = fixture.nativeElement.querySelector('#dni');
+    expect(dni).toBeTruthy();
+
+    //Codi de federat
+    const codi = fixture.nativeElement.querySelector('#codi');
+    expect(codi).toBeTruthy();
+
+    //Nom i cognoms
+    const nom = fixture.nativeElement.querySelector('#nom');
+    expect(nom).toBeTruthy();
+
+    //Telefon
+    const telefon = fixture.nativeElement.querySelector('#telefon');
+    expect(telefon).toBeTruthy();
+
+    //Email
+    const email = fixture.nativeElement.querySelector('#email');
+    expect(email).toBeTruthy();
+    
+  });
+  
 });
